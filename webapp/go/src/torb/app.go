dbf56f8c1de8db8346b32af2757d438f4b858daa
isucon <h1r0e44b11uk@gmail.com>
2019-08-25 15:57:08 +0800

    接続方法をunix domain socketへ変更

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -15,6 +15,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"net"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/sessions"
@@ -913,7 +914,23 @@ func main() {
 		return renderReportCSV(c, reports)
 	}, adminLoginRequired)
 
-	e.Start(":8080")
+	// ここからソケット接続設定 ---
+	socket_file := "/tmp/goecho.sock"
+	os.Remove(socket_file)
+
+	l, err := net.Listen("unix", socket_file)
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+	// go runユーザとnginxのユーザ（グループ）を同じにすれば777じゃなくてok
+	err = os.Chmod(socket_file, 0777)
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+
+	e.Listener = l
+
+	e.Start("")
 }
 
 type Report struct {
